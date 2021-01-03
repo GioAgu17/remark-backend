c4f5185d8e268c396c57fa0ba00f9b09595d5efd
GioAgu17 <giovanni.agugini@gmail.com>
2021-01-02 18:06:40 -0600

    Committing a return statement

--- services/offers/create.js
+++ services/offers/create.js
@@ -8,4 +8,5 @@ export const main = handler(async (event, context) => {
   const offerID = uuid.v1();
   const businessID = event.requestContext.identity.cognitoIdentityId;
   geospatial.insertOffer(data, businessID, offerID);
+  return data;
 });
