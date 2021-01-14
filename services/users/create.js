226c4632165107f3be756a64da6d902f0260bdfe
GioAgu17 <giovanni.agugini@gmail.com>
2021-01-14 01:34:33 -0600

    Committing create in user

--- services/users/create.js
+++ services/users/create.js
@@ -4,13 +4,13 @@ export const main = handler(async (event, context) => {
   const data = JSON.parse(event.body);
   console.log(data);
   const userID = event.requestContext.identity.cognitoIdentityId;
-  const params = {
+  /*const params = {
     TableName: process.env.userTableName,
     Item: {
       // The attributes of the item to be created
       userID : userId,
       createdAt: Date.now(), // Current Unix timestamp
     },
-  };
+  };*/
   return userID;
 });
