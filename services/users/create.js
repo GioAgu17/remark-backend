630acaa27307f6cd9143581cb767ae6e685910aa
GioAgu17 <giovanni.agugini@gmail.com>
2021-01-15 00:44:28 -0600

    Committing new method for user

--- services/users/create.js
+++ services/users/create.js
@@ -1,16 +1,22 @@
 import handler from "./libs/handler-lib";
+import AWS from "aws-sdk";
+import dynamoDb from "./libs/dynamodb-lib";
 
 export const main = handler(async (event, context) => {
   const data = JSON.parse(event.body);
   console.log(data);
   const userID = event.requestContext.identity.cognitoIdentityId;
-  /*const params = {
+  const userDetails = AWS.DynamoDB.Converter.input(data.userDetails);
+  const params = {
     TableName: process.env.userTableName,
     Item: {
       // The attributes of the item to be created
-      userID : userId,
-      createdAt: Date.now(), // Current Unix timestamp
+      userId : userID,
+      userType: data.userType,
+      userDetails: userDetails,
+      createdAt: Date.now(),
     },
-  };*/
+  };
+  await dynamoDb.put(params);
   return userID;
 });
