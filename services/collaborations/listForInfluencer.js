470f7612e8608875c41d9320c6d81d545bd68e45
GioAgu17 <giovanni.agugini@gmail.com>
2021-02-23 22:36:57 -0600

    Committing jobs to the pipeline activity

--- services/collaborations/listForInfluencer.js
+++ services/collaborations/listForInfluencer.js
@@ -1,6 +1,5 @@
 import handler from "../../libs/handler-lib";
 import dynamoDb from "../../libs/dynamodb-lib";
-
 export const main = handler(async (event, context) => {
   const params = {
     TableName: process.env.collaborationsTableName,
