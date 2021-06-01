b87c469b41e6c18360e5ae145abd55e4e24e6127
frankIT <fracchiaman@gmail.com>
2021-06-01 12:04:33 +0200

    Getting different req body in create user

--- services/users/create.js
+++ services/users/create.js
@@ -1,19 +1,27 @@
 import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
 export const main = handler(async (event, context) => {
-  const data = JSON.parse(event.body);
-  const userID = event.requestContext.identity.cognitoIdentityId;
-  const params = {
-    TableName: process.env.userTableName,
-    Item: {
-      // The attributes of the item to be created
-      userId : userID,
-      userType: data.userType,
-      loginInfo : data.loginInfo,
-      userDetails: data.userDetails,
-      createdAt: Date.now(),
-    },
-  };
-  await dynamoDb.put(params);
-  return { status: true };
+    const data = JSON.parse(event.body);
+    const userID = event.requestContext.identity.cognitoIdentityId;
+    const params = {
+        TableName: process.env.userTableName,
+        Item: {
+            // The attributes of the item to be created
+            userId : userID,
+            userType: data.userType,
+            loginInfo : {
+                email : data.email
+            },
+            userDetails: {
+                username : data.username,
+                accountIG : data.accountIG,
+                age : data.age,
+                caption : data.caption,
+                influencerCategories : data.influencerCategories,
+            },
+            createdAt: Date.now(),
+        }
+    };
+    await dynamoDb.put(params);
+    return { status: true };
 });
