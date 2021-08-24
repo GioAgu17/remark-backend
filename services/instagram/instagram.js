f2e333e7d0998f38dca4395460bab54fee242127
Giovanni Agugini Bassi <giovanni.agugini@gmail.com>
2021-08-23 22:21:24 -0500

    Dummy

--- services/instagram/instagram.js
+++ services/instagram/instagram.js
@@ -1,5 +1,6 @@
 import handler from "../../libs/handler-lib";
 export const main = handler(async (event, context) => {
   // add code here
+  console.log("Hello world");
   return { status: true };
 });
