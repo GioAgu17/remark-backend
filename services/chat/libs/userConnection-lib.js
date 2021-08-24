1af428afbde8547c29b1488e6d013f5a08a3d889
Giovanni Agugini Bassi <giovanni.agugini@gmail.com>
2021-08-24 11:57:49 -0500

    Update userConnection lib

--- services/chat/libs/userConnection-lib.js
+++ services/chat/libs/userConnection-lib.js
@@ -1,5 +1,5 @@
 import dynamoDb from "../../../libs/dynamodb-lib";
-import arrayHelper from "../../../libs/arrayHelper-lib";
+import * as arrayHelper from "../../../libs/arrayHelper-lib";
 export async function handleUserConnection(userId, connectionId){
     const readParams = {
         TableName: process.env.connectionChatTableName,
