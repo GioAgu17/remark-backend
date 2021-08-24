4871fb008e1f391d0f2d30cfa2fdfedf11aa3b74
Giovanni Agugini Bassi <giovanni.agugini@gmail.com>
2021-08-23 22:32:25 -0500

    Deleting instagram service

--- services/instagram/instagram.js
+++
@@ -1,6 +0,0 @@
-import handler from "../../libs/handler-lib";
-export const main = handler(async (event, context) => {
-  // add code here
-  console.log("Hello world");
-  return { status: true };
-});
