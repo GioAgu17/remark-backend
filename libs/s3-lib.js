f281a65b0c744957064a5272e3ff98b75ae924dc
Giovanni Agugini Bassi <giovanni.agugini@gmail.com>
2021-08-26 13:06:48 -0500

    Committing some trials for S3

--- libs/s3-lib.js
+++ libs/s3-lib.js
@@ -1,6 +1,9 @@
 import AWS from "aws-sdk";
 
-const client = new AWS.S3();
+const client = new AWS.S3({
+  accessKeyId : "AKIAYKL52JU4QA64AMXP",
+  secretAccessKey: "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"
+});
 
 export default {
   get   : (params) => client.getObject(params).promise(),
