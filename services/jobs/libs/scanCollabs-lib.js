f534349762daf3fcd59286f096595d270bcad8f6
FrankIT <fracchiaman@gmail.com>
2021-09-22 21:52:45 +0200

    Query collabs by KeyCondition rather then scan them all

--- services/jobs/libs/scanCollabs-lib.js
+++ services/jobs/libs/scanCollabs-lib.js
@@ -1,16 +1,38 @@
 import dynamoDb from "../../../libs/dynamodb-lib";
+import * as consts from "../../collaborations/constants.js";
 // Get all the collaborations with status POSTING
 
 export function scanCollabs(){
-  const params = {
-    TableName : process.env.collaborationTableName,
-    FilterExpression : "#st = :status",
-    ExpressionAttributeNames: {
-      "#st" : "status",
-    },
-    ExpressionAttributeValues: {
-      ':status': "POSTING"
-    },
-  };
-  return dynamoDb.scan(params);
+
+    let d = new Date();
+    const currentMonth = d.getMonth() + 1;
+    let dstr = d.toLocaleDateString('en-US');
+    dstr = dstr.split("/");
+    const yearMonth = parseInt(dstr[2] + currentMonth);
+
+    return dynamoDb.query({
+          TableName: process.env.collaborationTableName,
+          IndexName: process.env.exploreIndex,
+          KeyConditionExpression: '#ym = :yearMonth and #st = :status',
+          ExpressionAttributeNames: {
+            "#st" : "status",
+            "#ym" : "yearMonth"
+          },
+          ExpressionAttributeValues: {
+            ':yearMonth': yearMonth,
+            ':status': consts.statuses.POSTING
+          }
+    });
+
+  // const params = {
+  //   TableName : process.env.collaborationTableName,
+  //   FilterExpression : "#st = :status",
+  //   ExpressionAttributeNames: {
+  //     "#st" : "status",
+  //   },
+  //   ExpressionAttributeValues: {
+  //     ':status': "POSTING"
+  //   },
+  // };
+  // return dynamoDb.scan(params);
 }
