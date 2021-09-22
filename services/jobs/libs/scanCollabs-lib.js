0aa627d6948e95467c28a5d68c56c8f526fffa09
FrankIT <fracchiaman@gmail.com>
2021-09-22 12:21:18 +0200

    removed out of place constant

--- services/jobs/libs/scanCollabs-lib.js
+++ services/jobs/libs/scanCollabs-lib.js
@@ -1,12 +1,6 @@
 import dynamoDb from "../../../libs/dynamodb-lib";
 // Get all the collaborations with status POSTING
 
-export const statuses = {
-  INPROGRESS : "IN-PROGRESS",
-  POSTING: "POSTING",
-  POSTED : "POSTED",
-};
-
 export function scanCollabs(){
   const params = {
     TableName : process.env.collaborationTableName,
