19c2e11c44e22d7b3d94ec9f617704cb4085ab74
FrankIT <fracchiaman@gmail.com>
2021-09-30 12:30:54 +0200

    Take expiryDate only as param for relaunch.js

--- services/offers/relaunch.js
+++ services/offers/relaunch.js
@@ -1,30 +1,26 @@
 import handler from "../../libs/handler-lib";
-import dynamoDb from "../../libs/dynamodb-lib";
 import * as insert from "../../libs/insertOffer-lib";
 import * as deleteExpiredOffer from "./libs/deleteExpiredOffer-lib";
+import * as expiredOffers from "./expiredOffers.js";
+
 export const main = handler(async (event, context) => {
   const data = JSON.parse(event.body);
-  if(!data.offerId){
-    throw new Error("Cannot proceed without offerId");
-  }
+
   if(!data.expiryDate){
     throw new Error("Cannot proceed without expiryDate");
   }
-  const readParams = {
-    TableName: process.env.expiredOffersTable,
-    Key: {
-      "businessId": event.requestContext.identity.cognitoIdentityId,
-      "offerId": data.offerId
-    }
-  };
-  const result = await dynamoDb.get(readParams);
-  if(!result.Item){
-    return "Item not found";
+
+  let expiredoffers = await expiredOffers.main(event);
+  expiredoffers = JSON.parse(expiredoffers.body);
+  if(expiredoffers.length){
+      // Is it supposed to eventually return multiple results?
+      for(let offer of expiredoffers){
+          offer.offerDetails.expiryDate = data.expiryDate;
+          offer.latitude = offer.offerDetails.latitude;
+          offer.longitude = offer.offerDetails.longitude;
+          await insert.insertOffer(offer);
+          await deleteExpiredOffer.main(event.requestContext.identity.cognitoIdentityId, offer.offerId);
+      }
   }
-  result.Item.offerDetails.expiryDate = data.expiryDate;
-  result.Item.latitude = result.Item.offerDetails.latitude;
-  result.Item.longitude = result.Item.offerDetails.longitude;
-  await insert.insertOffer(result.Item);
-  await deleteExpiredOffer.main(event.requestContext.identity.cognitoIdentityId, data.offerId);
   return { status: true };
 });
