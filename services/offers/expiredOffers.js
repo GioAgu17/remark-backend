5527751e4463a27e564925910824f3d4f99f7cf9
FrankIT <fracchiaman@gmail.com>
2021-10-12 17:09:43 +0200

    Dynamic dynamo query/get method on getting expired collabs

--- services/offers/expiredOffers.js
+++ services/offers/expiredOffers.js
@@ -1,31 +1,33 @@
 import handler from "../../libs/handler-lib";
 import dynamoDb from "../../libs/dynamodb-lib";
 
-
 export const main = handler(async (event, context) => {
-  const data = JSON.parse(event.body);
-  const offerId = data.offerId;
-  const businessId = event.requestContext.identity.cognitoIdentityId;
 
-  let conditionExpr = 'businessId = :businessId';
-  let attrValues = { ':businessId': businessId };
+    const data = JSON.parse(event.body);
+    const offerId = data.offerId;
+    const businessId = event.requestContext.identity.cognitoIdentityId;
 
-  if(offerId){
-    conditionExpr = conditionExpr + ' and offerId = :offerId';
-    attrValues = { ...attrValues, ':offerId': offerId };
-  }
+    let expiredOffersParams = { TableName: process.env.expiredOffersTable };
 
-  const expiredOffersParams = {
-    TableName: process.env.expiredOffersTable,
-    KeyConditionExpression: conditionExpr,
-    ExpressionAttributeValues: attrValues
-  };
-  const result = await dynamoDb.query(expiredOffersParams);
-// just return an empty array
-// if(!result.Items){
-//   throw new Error("Expired offers not found");
-// }
-  const expiredOffers = result.Items;
-  console.log(expiredOffers);
-  return expiredOffers;
+    if(offerId){
+        expiredOffersParams.Key = {
+            'businessId': businessId,
+            'offerId' : offerId
+        };
+    }else{
+        expiredOffersParams.KeyConditionExpression = 'businessId = :businessId';
+        expiredOffersParams.ExpressionAttributeValues = {
+            ':businessId': businessId
+        };
+    }
+    const result = offerId ?
+        await dynamoDb.get(expiredOffersParams) :
+        await dynamoDb.query(expiredOffersParams);
+
+    const expiredOffers = offerId ?
+        ( result.Item ? [result.Item] : [] ) :
+        result.Items;
+
+    console.log(expiredOffers);
+    return expiredOffers;
 });
